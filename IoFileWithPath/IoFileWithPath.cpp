c9804e87e9db0bbeecab389f1f73dbf685d938e0
vararais <varadigmaisnann@gmail.com>
2024-06-04 17:54:38 +0700

    Membuat Io File With Path

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+using namespace std;
+
 int main()
 {
-    
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File: ";
+    cin >> NamaFile;
+
+    ofstream outfile;
+
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, 'q\' untuk keluar" << endl;
+
+    while (true) {
+        cout << "- ";
+
+        getline(cin, baris);
+
+        if (baris == "q") break;
+
+        outfile << baris << endl;
+    }
+    outfile.close();
+
+    ifstream infile;
+
+    infile.open(NamaFile + ".txt.",ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open()) {
+        while (getline(infile, baris)) {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
 }
 
 
