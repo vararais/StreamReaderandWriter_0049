31c81fcede117e07d44a8cc52dd941f10a592d78
vararais <varadigmaisnann@gmail.com>
2024-06-04 18:25:55 +0700

    Memanggil Elemen

--- Exception/Exception.cpp
+++ Exception/Exception.cpp
@@ -7,4 +7,18 @@ using namespace std;
 int main()
 {
 
+        cout << "Awal Program" << endl; 
+        try {
+            array<int, 3> data = { 6, 7, 8 };
+            cout << data.at(5) << endl;
+        }
+        catch (exception& e) {
+            
+            cout << e.what() << endl;
+            
+        }
+        cout << "Baris program yang terakhir" << endl;
+        
+            return 0;
+
 }
