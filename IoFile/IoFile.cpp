d60b426f42607af2892e13049f9e7becd0c88676
vararais <varadigmaisnann@gmail.com>
2024-06-04 17:34:04 +0700

    Menambahkan Elemen Pelengkap

--- IoFile/IoFile.cpp
+++ IoFile/IoFile.cpp
@@ -3,8 +3,25 @@
 #include <string>
 
 using namespace std;
+
 int main()
 {
-   
+	string baris;
+
+	ofstream outfile;
+
+	outfile.open("Contohfile.txt");
+
+	cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+
+	while (true) {
+		cout << "- ";
+
+		getline(cin, baris);
+
+		if (baris == "q") break;
+
+		outfile << baris << endl;
+	}
 }
 
